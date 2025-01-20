c24be11a119b7116cf434afbbe874f7b7d2909b9
Kai Ming <gkaim9@gmail.com>
2025-01-21 00:05:16 +0800

    update data for timeline component

--- app/data.ts
+++ app/data.ts
@@ -4,4 +4,23 @@ My goal is to leverage my technical skills to create innovative and exciting pro
 
 export const about: string = `I am a Biomedical  Engineering graduate from National University of Singapore (NUS). My passion for programming began when I was exposed to the world of programming through my modules. Through these modules, I discovered that I enjoyed creating innovative solutions that have real-world impact. My internship with Venture Corporation Ltd served to test my knowledge of programming and imbued me with knowledge of technologies involved in a collaborative environment, such as Git. I joined 42 Singapore as a student shortly after graduation to further my skills in an environment that fosters collaboration, continuous learning and cretive thinking.
 
-Whether it's building sleek and responsive interfaces, or developing robust backend systems, I am excited by the oppportunity to work on projects that have a meaningful impact on the world. My goal is to collaborate with like-minded individuals, learn from others, and to contribute to innovative projects that bridge the gap between technology and real-world applications.`
+Whether it's building sleek and responsive interfaces, or developing robust backend systems, I am excited by the oppportunity to work on projects that have a meaningful impact on the world. My goal is to collaborate with like-minded individuals, learn from others, and to contribute to innovative projects that bridge the gap between technology and real-world applications.`;
+
+export const timelineData = [
+  {
+    id: 1,
+    title: "Bachelor's Degree (National University of Singapore)",
+    location: "Singapore",
+    description:
+      "Graduated from NUS with a bachelor's degree in biomedical engineering",
+    date: "Aug 2019 - Jul 2023",
+  },
+  {
+    id: 2,
+    title: "Korea University",
+    location: "Seoul, South Korea",
+    description:
+      "Participated in Korea University's International Summer Campus as an NUS Summer Exchange student",
+    date: "Jun 2022 - Aug 2022",
+  },
+];
