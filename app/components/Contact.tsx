d0193fadceb61af76d0e8f0dffeccde6293b4081
Kai Ming <gkaim9@gmail.com>
2025-01-27 16:42:45 +0800

    updated contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { contactInfo } from "../data";
-import Link from "next/link";
 import { motion } from "framer-motion";
 
 const Contact = () => {
