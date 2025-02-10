9b79a2ca498c2b256958a86ce81d86ec79a6bc7f
Kai Ming <gkaim9@gmail.com>
2025-02-10 14:00:17 +0800

    edit to hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
             >
-              Aspiring Fullstack Programmer
+              Aspiring Programmer
             </motion.span>
             <motion.p
               variants={settings(1)}
