e13518545c62da66d7f32f6f39db6cb6dddc8bed
Kai Ming <gkaim9@gmail.com>
2026-03-18 14:32:18 +0800

    removed profile pic

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { summary } from "../data";
-import Image from "next/image";
-import profile_pic from "../../public/profile_pic.jpg";
 
 const Hero = () => {
   const settings = (delay: number) => ({
@@ -16,26 +14,28 @@ const Hero = () => {
   });
 
   return (
-    <div className="border-b border-neutral-900 border-transparent pb-4 px-4 lg:mb-35">
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
+    <div className="border-b border-neutral-900 pb-4 px-4 lg:mb-35">
+      <div className="flex justify-center">
+        <div className="w-full max-w-4xl">
           <div className="flex flex-col items-center lg:items-start">
             <motion.h1
               variants={settings(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl text-white"
+              className="pb-10 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl text-white text-center lg:text-left"
             >
               Goh Kai Ming
             </motion.h1>
+
             <motion.span
               variants={settings(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl lg:text-4xl tracking-tight text-transparent text-center lg:text-left"
             >
-              Aspiring Software Engineer/Software Developer
+              Aspiring Software Engineer / Software Developer
             </motion.span>
+
             <motion.p
               variants={settings(1)}
               initial="hidden"
@@ -44,24 +44,6 @@ const Hero = () => {
             >
               {summary}
             </motion.p>
-          </div>
-        </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex justify-center">
-            <motion.div
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 1.2 }}
-              className="rounded-2xl"
-            >
-              <Image
-                src={profile_pic}
-                alt="KM"
-                layout="intrinsic"
-                width={500}
-                height={500}
-              />
-            </motion.div>
           </div>
         </div>
       </div>
