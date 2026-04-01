2c48eadb68e9e4ecca86eef0b6c84e46297144d9
Kai Ming <gkaim9@gmail.com>
2026-04-01 13:43:14 +0800

    feat: added table for showcasing project
    - added a table in 42 projects to showcase a sample project for now

--- app/42_proj.ts
+++ app/42_proj.ts
@@ -2,6 +2,7 @@ export const proj_42 = [
     {
         name: "Libft",
         description: "Create a library of functions to be used in later projects",
-        techstack: ["C", "Linux", "Bash"]
-    }
+        techstack: ["C", "Linux", "Bash"],
+        link: "https://www.github.com/gka1m/42-core/",
+    },
 ];
